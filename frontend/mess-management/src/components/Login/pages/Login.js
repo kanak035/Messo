e3abf918ae85776339485e2d565690613d55472b
Kanak <kanakdhiman3669@gmail.com>
2025-02-13 21:45:46 +0530

    build

--- frontend/mess-management/src/components/Login/pages/Login.js
+++ frontend/mess-management/src/components/Login/pages/Login.js
@@ -1,6 +1,8 @@
 import Header from "../components/Header";
 import Login from "../components/Login";
 
+// login 
+
 export default function LoginPage() {
   return (
     <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-neutral-900">
